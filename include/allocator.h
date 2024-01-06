7c08383c3316b704b17d60fe167fbed198a0ede0
adrianwilhelmipwr <268479@student.pwr.edu.pl>
2024-01-06 12:26:01 +0100

    added align test

--- include/allocator.h
+++ include/allocator.h
@@ -6,6 +6,8 @@
 #include<unistd.h>
 #include<pthread.h>
 
+//#define allocate(bytes) allocate(bytes, __FILE__, __LINE__);
+
 typedef struct mem_chunk{
 	size_t size;
 	bool is_free;
@@ -16,10 +18,12 @@ extern memory_chunk*heap_head;
 extern pthread_mutex_t allocator_mutex;
 
 void initialize_allocator();
-void*allocate(size_t bytes);
+//void*allocate(size_t bytes, __FILE__, __LINE__);
+void*allocate(size_t bytes)
 void free(void*addr);
-void free_all();
 void dump_memory();
+
+void free_all();
 align(size_t*size);
 
 #endif
--- test/test_allign.c
+++ test/test_allign.c
@@ -1,1 +1,17 @@
+#include<assert.h>
+
 #include"allocator.h"
+
+void test_allign(){
+	size_t size = 7;
+	allign(&size);
+	assert(size == 8);
+	
+	size = 8;
+	allign(&size);
+	assert(size == 8);
+	
+	size = 9;
+	allign(&size);
+	assert(size == 16);
+}
